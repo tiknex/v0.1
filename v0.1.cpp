a3d51aa31d62f832297459d640576e8a608d1382
Vakaris <vakariopastas@gmail.comgit config --global user.name>
2026-09-22 23:03:42 +0300

    antras sinchronizavimas

--- v0.1.cpp
+++ v0.1.cpp
@@ -19,20 +19,36 @@ double vidurkis(int a, int b) {
     double v = (a + b) / 2.0;  
     return v;
 }
-int main() {
-    Studentas A;
-    cout<<"iveskite studento varda"<<endl;
-    cin>>A.vardas;
-    cout<<"iveskite studento pavarde"<<endl;
-    cin>>A.pavarde;
-    cout<<"iveskite studento n.d. pazymi"<<endl;
-    cin>>A.nd_rez;
-    cout<<"iveskite studento egzamino pazymi"<<endl;
-    cin>>A.egz_rez;
+double mediana(int a, int b){
 
-    double rezultatas = vidurkis(A.nd_rez, A.egz_rez);
-    //cout << fixed << setprecision(2);
-    cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutins (vid.)"<<endl;    
-    cout<<string(45, '-')<<endl;
-    cout<<left<<setw(15)<<A.pavarde<<setw(15)<<A.vardas<<setw(15)<<rezultatas; 
+}
+int main() {
+    int stud_kiekis;
+    cout<<"studentu skaicius"<<endl;
+    cin>>stud_kiekis;
+    Studentas A;
+    vector<Studentas> studentai;
+    for(int i = 0; i < stud_kiekis; i++){
+        cout<<"iveskite studento varda"<<endl;
+        cin>>A.vardas;
+        cout<<"iveskite studento pavarde"<<endl;
+        cin>>A.pavarde;
+        cout<<"iveskite studento n.d. pazymi"<<endl;
+        cin>>A.nd_rez;
+        cout<<"iveskite studento egzamino pazymi"<<endl;
+        cin>>A.egz_rez;
+        studentai.push_back(A);
+    }
+
+    cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis (vid.)"<<" / "<<"Galutinis (med.)"<<endl;    
+    cout<<string(70, '-')<<endl;
+    for(int i = 0; i < studentai.size(); i++){
+        double rezultatas = vidurkis(studentai[i].nd_rez, studentai[i].egz_rez);
+        //double mediana = 
+        cout << left << setw(15) << studentai[i].pavarde
+             << setw(15) << studentai[i].vardas
+             << setw(15) << rezultatas 
+             << endl;
+    }
+         
 }
