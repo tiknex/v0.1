1ad8f079990b5449c2a6324b5706d62c8b719192
Vakaris <vakariopastas@gmail.comgit config --global user.name>
2026-09-23 21:47:04 +0300

    6

--- v0.1.cpp
+++ v0.1.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 using std::cin;
 using std::cout;
@@ -15,45 +17,72 @@ struct Studentas{
     string vardas;
     string pavarde;
     vector<int> nd_rezultatai;
-    int egz_rez;
+    vector<int> egz_rezultatai;
 };
-double vidurkis(vector<int> nd, int egz) {
-    double suma = egz; 
+double vidurkis(vector<int> nd, vector<int> egz) {
+    double suma = 0; 
     for(int i=0; i < nd.size(); i++){
         suma += nd[i];
     }
-    return suma / (nd.size()+1);
+    for (int i = 0; i < egz.size(); i++) {
+        suma += egz[i];
+    }
+    return suma / (nd.size()+egz.size());
+}
+double mediana(){
+    
 }
 
 int main() {
+    srand(time(0));
+    string atsakymas;
     int stud_kiekis;
     cout<<"studentu skaicius"<<endl;
     cin>>stud_kiekis;
-    Studentas A;
     vector<Studentas> studentai;
     for(int i = 0; i < stud_kiekis; i++){
+        Studentas A;
         cout<<"iveskite studento varda"<<endl;
         cin>>A.vardas;
         cout<<"iveskite studento pavarde"<<endl;
         cin>>A.pavarde;
-        cout<<"iveskite studento n.d. pazymius, kai baigsite iveskite -1"<<endl;
-        while(true){
-            int reiksme;
-            cin>>reiksme;
-            if (reiksme == -1){
-                break;
-            }
-        }
-        cout<<"iveskite studento egzamino pazymi"<<endl;
-        cin>>A.egz_rez;
-        studentai.push_back(A);
+        cout<<"ar norite sugeneruoti nd ir egazmino pazymius atsitiktinai taip/ne?"<<endl;
+        cin>>atsakymas;
+    if (atsakymas == "taip") {
+        int kiekis_nd;
+        int kiekis_egz;
+        cout << "kiek nd pazymiu norite sugeneruoti?" << endl;
+        cin >> kiekis_nd;
+        cout<< "kiek egzamino pazymiu norite sugenruoti?" <<endl;
+        cin>>kiekis_egz;
+
+    for (int j = 0; j < kiekis_nd; j++) {
+        int atsitiktinis_skaicius_nd = rand() % 10 + 1;
+        A.nd_rezultatai.push_back(atsitiktinis_skaicius_nd);
     }
+    for (int j = 0; j < kiekis_egz; j++) {
+        int atsitiktinis_skaicius_egz = rand() % 10;
+        A.egz_rezultatai.push_back(atsitiktinis_skaicius_egz);
+    }   
+    } else {
+    cout << "iveskite studento nd pazymius, kai baigsite iveskite -1" << endl;
+    while (true) {
+        int reiksme;
+        cin >> reiksme;
+        if (reiksme == -1) {
+            break;
+        }
+        A.nd_rezultatai.push_back(reiksme);
+    }
+    }
+    studentai.push_back(A);
+}
 
     cout<<left<<setw(15)<<"Pavarde"<<setw(15)<<"Vardas"<<setw(15)<<"Galutinis(vid.)"<<"/"<<"Galutinis(med.)"<<endl;    
     cout<<string(70, '-')<<endl;
     for(int i = 0; i < studentai.size(); i++){
-        double rezultatas1 = vidurkis(studentai[i].nd_rezultatai, studentai[i].egz_rez);
-        double rezultatas2 = vidurkis(studentai[i].nd_rezultatai, studentai[i].egz_rez);
+        double rezultatas1 = vidurkis(studentai[i].nd_rezultatai, studentai[i].egz_rezultatai);
+        double rezultatas2 = vidurkis(studentai[i].nd_rezultatai, studentai[i].egz_rezultatai);
         cout <<left<<setw(15)<<studentai[i].pavarde
              <<setw(15)<<studentai[i].vardas
              <<setw(15)<<rezultatas1
